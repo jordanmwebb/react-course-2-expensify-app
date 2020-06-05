a6215b03b9a6b85fb58fba7706624d5d1a398145
Jordan Webb <jordanwebb@Jordans-MBP.attlocal.net>
2020-06-05 10:46:38 -0400

    Setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,53 @@
 const path = require('path');
+const ExtractTextPlugin = require('mini-css-extract-plugin');
 
-console.log(path.join(__dirname, 'public'));
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin({
+        filename: 'styles.css'
+    });
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [{
-           loader: 'babel-loader',
-           test: /\.js$/,
-           exclude: /node_modules/ 
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    }
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/ 
+            }, {
+                test: /\.s?css$/,
+                use: [
+                        ExtractTextPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ],
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
+    };
 };
+
+
 
